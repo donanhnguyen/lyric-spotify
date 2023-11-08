2a776ac22133a6cda5ae9b6ee7b68e470ad6da5e
Don Anh Nguyen <thedonwind69@gmail.com>
2023-11-08 11:21:55 -0600

    fixed issue with auth url

--- client/src/GlobalContext.js
+++ client/src/GlobalContext.js
@@ -4,16 +4,20 @@ const GlobalContext = createContext();
 
 export function GlobalProvider( {children} ) {
 
-    let renderURL = "";
+    let renderURL;
+    let SpotifyRedirectURI;
     if (process.env.NODE_ENV === "development") {
         renderURL = "http://localhost:3001";
+        SpotifyRedirectURI = "http://localhost:3000"
     } else if (process.env.NODE_ENV === 'production') {
         renderURL = "https://lyric-spotify-backend.onrender.com";
+        SpotifyRedirectURI = "https://lyric-spotify.onrender.com";
     };
     
     return (
         <GlobalContext.Provider value={ {
             renderURL,
+            SpotifyRedirectURI
         } }>
             {children}
         </GlobalContext.Provider>
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import {useContext} from 'react'
 import {Container, Image} from 'react-bootstrap';
-
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=3762e76b0589453c882fd03dc454aa60&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+import GlobalContext from './GlobalContext';
 
 export default function Login() {
+
+  const {SpotifyRedirectURI} = useContext(GlobalContext);
+
+  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=3762e76b0589453c882fd03dc454aa60&response_type=code&redirect_uri=${SpotifyRedirectURI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+
   return (
     <Container className='d-flex flex-column justify-content-center align-items-center' style={{ minHeight: '100vh' }}>
       <Image src={require('./spotifylogo.jpg')} alt="Spotify Logo" fluid /> {/* Add the image here */}
